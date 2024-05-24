a84732ae4f50e2b8af66ec3382d37c0c3081b4b3
Drozdaaa <krzys6d@gmail.com>
2024-05-24 17:50:11 +0200

    autoryzacja

--- routes/web.php
+++ routes/web.php
@@ -16,26 +16,26 @@ Route::controller(WholesalerController::class)->group(function(){
     Route::get('/hurtownia','index')->name('hurtownia.index');
 });
 Route::controller(ShopController::class)->group(function(){
-    Route::get('/sklepy','index')->name('shops.index');
-    Route::get('/shops/{id}/edit', 'edit')->name('shops.edit');
-    Route::put('/shops/{id}', 'update')->name('shops.update');
+    Route::get('/sklepy','index')->name('shops.index')->middleware('auth');
+    Route::get('/shops/{id}/edit', 'edit')->name('shops.edit')->middleware('auth');
+    Route::put('/shops/{id}', 'update')->name('shops.update')->middleware('auth');
 });
 Route::controller(supplierController::class)->group(function(){
-    Route::get('/suppliers','index')->name('suppliers.index');
-    Route::get('/suppliers/{id}/edit', 'edit')->name('suppliers.edit');
-    Route::put('/suppliers/{id}', 'update')->name('suppliers.update');
+    Route::get('/suppliers','index')->name('suppliers.index')->middleware('auth');
+    Route::get('/suppliers/{id}/edit', 'edit')->name('suppliers.edit')->middleware('auth');
+    Route::put('/suppliers/{id}', 'update')->name('suppliers.update')->middleware('auth');
 });
 
 Route::controller(employeeController::class)->group(function(){
-    Route::get('/employees','index')->name('employees.index');
-    Route::get('/employees/{id}/edit', 'edit')->name('employees.edit');
-    Route::put('/employees/{id}', 'update')->name('employees.update');
+    Route::get('/employees','index')->name('employees.index')->middleware('auth');
+    Route::get('/employees/{id}/edit', 'edit')->name('employees.edit')->middleware('auth');
+    Route::put('/employees/{id}', 'update')->name('employees.update')->middleware('auth');
 });
 
 Route::controller(orderController::class)->group(function(){
-    Route::get('/orders','index')->name('orders.index');
-    Route::get('/orders/{id}/edit', 'edit')->name('orders.edit');
-    Route::put('/orders/{id}', 'update')->name('orders.update');
+    Route::get('/orders','index')->name('orders.index')->middleware('auth');
+    Route::get('/orders/{id}/edit', 'edit')->name('orders.edit')->middleware('auth');
+    Route::put('/orders/{id}', 'update')->name('orders.update')->middleware('auth');
 
 });
 Route::controller(AuthController::class)->group(function () {
